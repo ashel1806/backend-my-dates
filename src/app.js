07d74e20486f4d153928afa81ab06396be2a95b5
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-06-06 01:30:11 -0500

    config: enabled cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
+// config
 import config from './config/config';
-import logger from './middlewares/logger';
 
+// routes
 import User from './api/routes/user.route';
+
+// middlewares
+import logger from './middlewares/logger';
 import requestLogger from './middlewares/requestLogger';
 import unknownEndpoint from './middlewares/unknownEndpoint';
 import errorHandler from './middlewares/errorHandler';
@@ -23,12 +28,14 @@ const connectDB = async () => {
 
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(requestLogger);
 
 app.use('/api/v1/auth/register', User);
 app.use('/ping', (req, res) => res.send('pong!'));
+app.use('/', (req, res) => res.send('Hi!'));
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
