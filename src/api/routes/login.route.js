8e34f6b44e1fda8b3dd9a8ee6fd393bc047a6585
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-06-20 03:37:00 -0500

    fix: Fixing the google login by adding your route

--- src/api/routes/login.route.js
+++ src/api/routes/login.route.js
@@ -4,5 +4,6 @@ import { LoginCtrl } from '../controllers';
 const router = new Router();
 
 router.route('/').post(LoginCtrl.apiLoginUser);
+router.route('/google').post(LoginCtrl.apiGoogleLogin);
 
 export default router;
