aaded7828c19285e205b662fdcbd2e0d37bb40b1
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-06-20 03:27:44 -0500

    feat: Now users can loging with Google

--- src/api/controllers/login.controller.js
+++ src/api/controllers/login.controller.js
@@ -1,5 +1,9 @@
 import bcrypt from 'bcrypt';
+import { OAuth2Client } from 'google-auth-library';
+
 import User from '../../models/User';
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export default class LoginController {
   // eslint-disable-next-line consistent-return
@@ -20,6 +24,28 @@ export default class LoginController {
       if (!passwordCorrect) {
         throw new Error('invalidPassword');
       }
+
+      res.status(200).send({
+        data: user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async apiGoogleLogin(req, res, next) {
+    try {
+      const { token } = req.body;
+
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      const { email } = ticket.getPayload();
+
+      const user = User.findOne({ email });
+
+      if (!user) throw new Error('invalidUser');
 
       res.status(200).send({
         data: user,
